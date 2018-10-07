b816a1abed4f695e10f04bb2f8f0eded9c8efe39
Nicholas Evans <nevans8@masonlive.gmu.edu>
2018-10-07 07:33:47 -0700

    "yet another"

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,6 @@
 var Migrations = artifacts.require("./Migrations.sol");
-
+var cdpcds = artifacts.require("./cdpcds.sol");
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(cdpcds);
 };
