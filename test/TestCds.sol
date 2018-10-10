3ae0f6fb6ebb007288a9ad02e2a433155e5ea49c
Nicholas Evans <nevans8@masonlive.gmu.edu>
2018-10-09 21:43:25 -0700

    "testing"

---
+++ test/TestCds.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/cdpcds.sol";
+
+contract TestCds{
+    function testInitilBalance() public{
+        cdpcds cdp = cdpcds(DeployedAddresses.cdpcds());
+        uint expected = 0;
+        Assert.equal(address(cdp).balance, expected, "Owner should have 10000 MetaCoin initially");
+    }
+}
